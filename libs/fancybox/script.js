6990d2f46fba935d262afeb3518da490e06c345b
Dmitriy <dmitriy.basko@gmail.com>
2017-10-11 17:45:37 +0300

    delete initial module files

--- libs/fancybox/script.js
+++
@@ -1,9 +0,0 @@
-jQuery(function(){
-    jQuery(".on-fancybox").fancybox({
-        tpl : {
-        	error    : '<p class="fancybox-error">Запрошенное содержимое не может быть загружен.<br/>Пожалуста попробуйте поже.</p>',
-        	closeBtn : '<a title="Закрыть" class="fancybox-item fancybox-close" href="javascript:;"><span class="mif-cross"></span></a>'
-         }
-        
-    });
-});
